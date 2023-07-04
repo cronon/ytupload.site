ea2e7c9120ddfea6602536e1ec1bb3d8b8856a10
cronon <cronon12@gmail.com>
2023-07-04 10:23:38 +0400

    remove song

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -64,12 +64,18 @@ interface Mp3ListProps {
     onChange: (newFiles: Mp3File[]) => void
 }
 function Mp3List({mp3s, onChange}: Mp3ListProps) {
+    const removeSong = (index: number) => {
+        const toRemove = [...mp3s];
+        toRemove.splice(index, 1);
+        onChange(toRemove)
+    }
     return <div className="mp3List">
         {mp3s.map((mp3, index) => {
             const songName = mp3.name.split('.mp3')[0]!
             return <div className="song" key={index}>
             {songName}{'\t'}
             <span className="song-duration">{mp3.duration}</span>
+            <button onClick={() => removeSong(index)}>x</button>
             </div>
         })}
     </div>
